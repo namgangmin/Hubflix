a00781cf98ff89bdc2181c1bc474cb9b684bf4f1
namgangmin <taeyoun522@gmail.com>
2024-05-02 01:05:14 +0900

    HTML 파일 추가 후 연동 테스트 진행

--- django/Hubflix/app/views.py
+++ django/Hubflix/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+def main_page(request):
+    # HTML 파일에 넘겨줄 데이터 정의
+    
+    
+    # HTML 파일에 넘겨줄 추가 내역들 삽입하는 곳
+    context = {
+        "now": "now"
+    } 
+    
+    # request에 대해 main.html로 context데이터를 넘겨준다.
+    return render(request, 'main_page.html', context)
